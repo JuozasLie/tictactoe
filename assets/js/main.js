7174575f9612c97d7889f05f2f670a81b7c3b82b
Juozas Liebus <Juozas.liebus147@gmail.com>
2021-07-01 18:53:31 +0300

    game logic done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,9 @@
-const playerStatus = document.querySelector('#player');
-const turnStatus = document.querySelector('#turn-number')
-const winnerStatus = document.querySelector('#game-result');
-
-let gameActive = true;
-let currentPlayer = "X";
-let turnNumber = 1;
-
-let displayCurrentPlayer = () => playerStatus.innerHTML = currentPlayer;
-let displayTurnNumber = () => turnStatus.innerHTML = turnNumber;
-let displayWinner = () => {
-    winnerStatus.innerHTML = "The winner is ${currentPlayer}!";
-    toggleModal();
-}
-let displayDraw = () => {
-    winnerStatus.innerHTML = "The game ended in a draw!"
-    toggleModal();
-}
-displayTurnNumber();
-
 //modal
 const modal = document.querySelector(".modal");
 const closeButton = document.querySelector(".close-button");
 function toggleModal() {
     modal.classList.toggle("show-modal");
 }
-
 function windowOnClick(event) {
     if (event.target === modal) {
         toggleModal();
@@ -34,11 +13,110 @@ closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 //end modal
 
+//DOM elements we will use
+const playerStatus = document.querySelector('#player');
+const turnStatus = document.querySelector('#turn-number')
+const winnerStatus = document.querySelector('#game-result');
+document.querySelectorAll('.cell').forEach(cell =>
+    cell.addEventListener('click', handleCellClick));
+document.querySelector('#reset-button').addEventListener('click', restartGame);
+
+//Winning combinations
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [3, 5, 7]
+];
+
+//Starting variables
+let gameActive = true;
+let currentPlayer = "X";
+let turnNumber = 1;
+let gameState = ["","","","","","","","","",""];
+
+//DOM manipulation
+let displayCurrentPlayer = () => playerStatus.innerHTML = currentPlayer;
+let displayTurnNumber = () => turnStatus.innerHTML = turnNumber;
+let displayWinner = () => {
+    winnerStatus.innerHTML = `The winner is ${currentPlayer}!`;
+    toggleModal();
+}
+let displayDraw = () => {
+    winnerStatus.innerHTML = "The game ended in a draw!"
+    toggleModal();
+}
+let fillCell = (cell) => {
+    let img = document.createElement('img');
+    img.src = `/assets/images/${currentPlayer}.png`
+    cell.appendChild(img);
+}
+displayTurnNumber();
+
+//game logic
 function changeActivePlayer() {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
     displayCurrentPlayer();
-}
+};
 function incrementTurnNumber(){
     turnNumber++;
     displayTurnNumber();
+};
+function handleCellClick(clickEvent){
+    if(gameActive){
+        const clickedCell = clickEvent.target;
+        const clickedCellIndex = parseInt(clickedCell.getAttribute("data-cell-index"));
+        if(gameState[clickedCellIndex] !== "" || !gameActive){
+            return;
+        }
+        handleCellPlayed(clickedCell, clickedCellIndex);
+    }
 }
+function handleCellPlayed(clickedCell, clickedCellIndex){
+    gameState[clickedCellIndex] = currentPlayer;
+    fillCell(clickedCell);
+    handleVictoryValidation();
+}
+function handleVictoryValidation(){
+    let roundWon = false;
+    for(let i = 0; i <= 8; i++){
+        let victoryCondition = winningConditions[i];
+        let firstCell = gameState[victoryCondition[0]];
+        let secondCell = gameState[victoryCondition[1]];
+        let thirdCell = gameState[victoryCondition[2]];
+        if(firstCell == "" || secondCell == "" || thirdCell == ""){
+            continue;
+        } else if(firstCell == secondCell && secondCell == thirdCell){
+            roundWon = true;
+            break;
+        } 
+    }
+    if(roundWon){
+        displayWinner();
+        gameActive = false;
+        return;
+    }
+    let roundDraw = !gameState.includes('');
+    if(roundDraw){
+        displayDraw();
+        gameActive = false;
+        return;
+    }
+    changeActivePlayer();
+    incrementTurnNumber();
+}
+function restartGame(){
+    document.querySelectorAll('.cell').forEach(cell =>
+        cell.innerHTML = "");
+    gameActive = true;
+    currentPlayer = "X";
+    turnNumber = 1;
+    gameState = ["","","","","","","","","",""];
+    displayTurnNumber();
+    displayCurrentPlayer();
+};
